5566928b34f7b5f25ffbcd1850c059714d10afbf
Aayman-star <aayman.khalid1@gmail.com>
2024-04-18 22:48:00 +0500

    added key to the links map

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,31 +14,34 @@ const Header = () => {
   return (
     <div className="w-full p-4 border-b-2 border-slate-50">
       <div className="flex items-center justify-between">
-        <h1 className={`text-xl font-semibold ${navBar ? "hidden" : "block"}`}>
-          Kanza Khalid
-        </h1>
+        <h1 className={`text-xl font-semibold `}>Kanza Khalid</h1>
         <div className="flex items-center gap-2">
           {darkMode ? (
             <MoonStar
               onClick={() => setDarkMode(false)}
-              className="text-slate-50 text-2xl font-bold"
+              className={`text-slate-50 text-2xl font-bold ${
+                navBar ? "hidden" : "block"
+              }`}
             />
           ) : (
             <SunMedium
               onClick={() => setDarkMode(true)}
-              className={`text-slate-50 text-2xl font-bold
+              className={`text-slate-50 text-2xl font-bold ${
+                navBar ? "hidden" : "block"
               }`}
             />
           )}
           {navBar ? (
-            <div className="bg-blue-500 fixed left-0 top-0 w-[100%]  h-screen ">
+            <div className="fixed left-0 top-0 w-[100%]   h-screen z-10 ">
               <X
                 onClick={() => setNavBar(!navBar)}
                 className="text-slate-50 text-2xl font-bold absolute top-5 right-5"
               />
-              <ul className="bg-green-500  h-screen  p-2 flex flex-col items-center space-y-6  mt-24">
+              <ul className="h-screen  p-2 flex flex-col items-center space-y-6  mt-24">
                 {navLinks.map((link, index) => (
-                  <li className="hover:scale-105 hover:underline">{link}</li>
+                  <li key={index} className="hover:scale-105 hover:underline">
+                    {link}
+                  </li>
                 ))}
               </ul>
             </div>
