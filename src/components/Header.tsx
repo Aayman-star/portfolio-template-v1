8834e2d26d35f9804f9dbd9087410062707941b9
Aayman-star <aayman.khalid1@gmail.com>
2024-04-19 22:33:24 +0500

    Made the mobile menu working

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { Menu, X, SunMedium, MoonStar } from "lucide-react";
 import { useTheme } from "next-themes";
-
+import { Button } from "./ui/button";
+import Link from "next/link";
 import { useState } from "react";
 const Header = () => {
-  const { setTheme } = useTheme();
-  const [darkMode, setDarkMode] = useState(false);
+  const { theme, setTheme } = useTheme();
+
   const [navBar, setNavBar] = useState(false);
   const navLinks = [
     "Home",
@@ -14,42 +15,40 @@ const Header = () => {
     "Achievements",
     "Skills",
   ];
-  const handleTheme = () => {
-    setDarkMode(!darkMode);
-    setTheme(darkMode ? "light" : "dark");
-  };
+
   return (
-    <div className="bg-background w-full p-4 ">
+    <nav className="bg-background w-full p-4 z-10 sticky">
       <div className="flex items-center justify-between">
-        <h1 className={`text-xl font-bold text-primary`}>Kanza Khalid</h1>
-        <div className="flex items-center gap-2">
-          {darkMode ? (
-            <SunMedium
-              onClick={handleTheme}
-              className={`text-foreground text-2xl font-bold ${
-                navBar ? "hidden" : "block"
-              }`}
-            />
-          ) : (
-            <MoonStar
-              onClick={handleTheme}
-              className={`text-foreground text-2xl font-bold ${
-                navBar ? "hidden" : "block"
-              }`}
-            />
-          )}
+        <h1
+          className={`text-2xl font-extrabold text-primary ${
+            navBar && "z-10"
+          }`}>
+          Kanza Khalid
+        </h1>
+        <div className={`flex items-center justify-between`}>
+          <Button
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            variant="ghost"
+            size="sm"
+            className={`${navBar ? "hidden" : "block"}`}>
+            {theme === "dark" ? <SunMedium /> : <MoonStar />}
+          </Button>
 
           {navBar ? (
-            <div className="fixed left-0 top-0 w-[100%]   h-screen z-10 ">
+            <div className="fixed left-0 top-0  w-[100%]  min-h-screen bg-background">
               <X
                 onClick={() => setNavBar(!navBar)}
                 className="text-foreground text-2xl font-bold absolute top-5 right-5"
               />
-              <ul className="h-screen  p-2 flex flex-col items-center space-y-6  mt-28">
+              <ul className="w-full h-screen  z-10 overflow-x-hidden  p-2 flex flex-col items-center space-y-6  mt-28">
                 {navLinks.map((link, index) => (
-                  <li key={index} className="hover:scale-105 hover:underline">
-                    {link}
-                  </li>
+                  <Link href="#">
+                    <li
+                      key={index}
+                      className="hover:scale-110 hover:underline hover:text-primary">
+                      {link.toUpperCase()}
+                    </li>
+                  </Link>
                 ))}
               </ul>
             </div>
@@ -61,7 +60,7 @@ const Header = () => {
           )}
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
---
+++ src/components/MainApp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainApp = () => {
+  return (
+    <div className="w-full min-h-screen p-4">
+      <h1 className="text-3xl font-semibold text-foreground">
+        This is the main app
+      </h1>
+    </div>
+  );
+};
+
+export default MainApp;
